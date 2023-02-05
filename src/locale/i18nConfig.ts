02eb569a30bb45b344595a9ca0726733f7f8f2b9
nicolas-meilan <sebastian.nicolas.meilan@gmail.com>
2023-02-05 02:56:42 -0300

    save current language on storage

--- src/locale/i18nConfig.ts
+++ src/locale/i18nConfig.ts
@@ -1,11 +1,13 @@
 import 'intl-pluralrules';
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import en from './en.json';
 import es from './es.json';
 import { getDeviceLocale } from '@system/deviceInfo';
+import StorageKeys from '@system/storageKeys';
 
 export enum LanguageLocale {
   ES = 'es',
@@ -19,10 +21,16 @@ export type LanguageLocaleType = Exclude<LanguageLocale, LanguageLocale.FROM_DEV
 
 
 const initializeI18nConfig = async () => {
+  const storedLng = await AsyncStorage.getItem(StorageKeys.LANGUAGE);
+  
   const deviceLocale = getDeviceLocale().split('_')[0];
+  const baseLng = Object.values(LanguageLocale).includes(deviceLocale as LanguageLocale)
+    ? deviceLocale : defaultLocale;
+
+  const lng = storedLng ? storedLng : baseLng;
 
   await i18next.use(initReactI18next).init({
-    lng: deviceLocale,
+    lng,
     resources: {
       [LanguageLocale.EN]: { translation: en },
       [LanguageLocale.ES]: { translation: es },
