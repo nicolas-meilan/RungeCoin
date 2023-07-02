66c19c70340e01c4e18025d0bdf7414f815088c7
nicolas-meilan <sebastian.nicolas.meilan@gmail.com>
2023-07-02 01:59:39 -0300

    Add message release on release github action

--- scripts/upload-build-production-flow.sh
+++ scripts/upload-build-production-flow.sh
@@ -1,3 +1,26 @@
+#!/bin/bash
+
+unset -v message
+
+while getopts m: flag
+do
+    case "${flag}" in
+      m) message=${OPTARG};;
+    esac
+done
+
+shift "$(( OPTIND - 1 ))"
+
+if [ -z "$message" ]; then
+  appcenter distribute stores publish\
+    -f ./android/app/build/outputs/bundle/release/app-release.aab\
+    --store Production
+  exit 0
+fi
+
+echo "App center message:"
+echo "$message"
 appcenter distribute stores publish\
   -f ./android/app/build/outputs/bundle/release/app-release.aab\
-  --store Production
+  --store Production\
+  --release-notes "$message"
